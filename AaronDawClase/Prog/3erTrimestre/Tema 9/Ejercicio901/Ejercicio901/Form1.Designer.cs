ab87ef283049f6280afc7a1cc63f37e19837d579
SanSteAaron <SanSteAaron@gmail.com>
2022-05-12 17:44:34 +0200

    12/05 Ejercicio901 Prog

---
+++ AaronDawClase/Prog/3erTrimestre/Tema 9/Ejercicio901/Ejercicio901/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿
+namespace Ejercicio901
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TBDni = new System.Windows.Forms.TextBox();
+            this.TBNombre = new System.Windows.Forms.TextBox();
+            this.TBTelefono = new System.Windows.Forms.TextBox();
+            this.TBApellidos = new System.Windows.Forms.TextBox();
+            this.TBEmail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.BPrimero = new System.Windows.Forms.Button();
+            this.BAnterior = new System.Windows.Forms.Button();
+            this.BSiguiente = new System.Windows.Forms.Button();
+            this.BÚltimo = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TBDni
+            // 
+            this.TBDni.Location = new System.Drawing.Point(166, 56);
+            this.TBDni.Name = "TBDni";
+            this.TBDni.Size = new System.Drawing.Size(141, 20);
+            this.TBDni.TabIndex = 0;
+            // 
+            // TBNombre
+            // 
+            this.TBNombre.Location = new System.Drawing.Point(166, 126);
+            this.TBNombre.Name = "TBNombre";
+            this.TBNombre.Size = new System.Drawing.Size(141, 20);
+            this.TBNombre.TabIndex = 1;
+            // 
+            // TBTelefono
+            // 
+            this.TBTelefono.Location = new System.Drawing.Point(166, 201);
+            this.TBTelefono.Name = "TBTelefono";
+            this.TBTelefono.Size = new System.Drawing.Size(141, 20);
+            this.TBTelefono.TabIndex = 2;
+            // 
+            // TBApellidos
+            // 
+            this.TBApellidos.Location = new System.Drawing.Point(496, 126);
+            this.TBApellidos.Name = "TBApellidos";
+            this.TBApellidos.Size = new System.Drawing.Size(141, 20);
+            this.TBApellidos.TabIndex = 3;
+            // 
+            // TBEmail
+            // 
+            this.TBEmail.Location = new System.Drawing.Point(496, 201);
+            this.TBEmail.Name = "TBEmail";
+            this.TBEmail.Size = new System.Drawing.Size(141, 20);
+            this.TBEmail.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(126, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Dni";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tlf";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(424, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Apellidos";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(442, 208);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "email";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.BÚltimo);
+            this.groupBox1.Controls.Add(this.BSiguiente);
+            this.groupBox1.Controls.Add(this.BAnterior);
+            this.groupBox1.Controls.Add(this.BPrimero);
+            this.groupBox1.Location = new System.Drawing.Point(166, 263);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 150);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Navegar";
+            // 
+            // BPrimero
+            // 
+            this.BPrimero.Location = new System.Drawing.Point(34, 44);
+            this.BPrimero.Name = "BPrimero";
+            this.BPrimero.Size = new System.Drawing.Size(75, 23);
+            this.BPrimero.TabIndex = 0;
+            this.BPrimero.Text = "Primero";
+            this.BPrimero.UseVisualStyleBackColor = true;
+            this.BPrimero.Click += new System.EventHandler(this.BPrimero_Click);
+            // 
+            // BAnterior
+            // 
+            this.BAnterior.Location = new System.Drawing.Point(140, 44);
+            this.BAnterior.Name = "BAnterior";
+            this.BAnterior.Size = new System.Drawing.Size(75, 23);
+            this.BAnterior.TabIndex = 1;
+            this.BAnterior.Text = "Anterior";
+            this.BAnterior.UseVisualStyleBackColor = true;
+            this.BAnterior.Click += new System.EventHandler(this.BAnterior_Click);
+            // 
+            // BSiguiente
+            // 
+            this.BSiguiente.Location = new System.Drawing.Point(244, 44);
+            this.BSiguiente.Name = "BSiguiente";
+            this.BSiguiente.Size = new System.Drawing.Size(75, 23);
+            this.BSiguiente.TabIndex = 2;
+            this.BSiguiente.Text = "Siguiente";
+            this.BSiguiente.UseVisualStyleBackColor = true;
+            this.BSiguiente.Click += new System.EventHandler(this.BSiguiente_Click);
+            // 
+            // BÚltimo
+            // 
+            this.BÚltimo.Location = new System.Drawing.Point(348, 44);
+            this.BÚltimo.Name = "BÚltimo";
+            this.BÚltimo.Size = new System.Drawing.Size(75, 23);
+            this.BÚltimo.TabIndex = 3;
+            this.BÚltimo.Text = "Último";
+            this.BÚltimo.UseVisualStyleBackColor = true;
+            this.BÚltimo.Click += new System.EventHandler(this.BÚltimo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TBEmail);
+            this.Controls.Add(this.TBApellidos);
+            this.Controls.Add(this.TBTelefono);
+            this.Controls.Add(this.TBNombre);
+            this.Controls.Add(this.TBDni);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TBDni;
+        private System.Windows.Forms.TextBox TBNombre;
+        private System.Windows.Forms.TextBox TBTelefono;
+        private System.Windows.Forms.TextBox TBApellidos;
+        private System.Windows.Forms.TextBox TBEmail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button BÚltimo;
+        private System.Windows.Forms.Button BSiguiente;
+        private System.Windows.Forms.Button BAnterior;
+        private System.Windows.Forms.Button BPrimero;
+    }
+}
+
---
+++ AaronDawClase/Prog/3erTrimestre/Tema 9/Ejercicio901/Ejercicio901/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace Ejercicio901
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DataSet dsProfesores;
+        SqlDataAdapter da;
+        private int pos;
+        private int maxRegistros;
+        private void mostrarRegistro(int pos)
+        {
+            DataRow dRegistro;
+            
+            dRegistro = dsProfesores.Tables["Profesores"].Rows[pos];
+           
+            TBDni.Text = dRegistro["DNI"].ToString();
+            TBNombre.Text = dRegistro["Nombre"].ToString();
+            TBApellidos.Text = dRegistro["Apellido"].ToString();
+            TBTelefono.Text = dRegistro["Tlf"].ToString();
+            TBEmail.Text = dRegistro["EMail"].ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cadenaConexión = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\sanchez22014\\Desktop\\AaronDawClase\\Prog\\3erTrimestre\\Instituto.mdf;Integrated Security=True;Connect Timeout=30";
+            
+            SqlConnection con = new SqlConnection(cadenaConexión);
+
+            con.Open();
+
+            string cadenaSQL = "SELECT * From Profesores";
+            da = new SqlDataAdapter(cadenaSQL, con);
+            dsProfesores = new DataSet();
+            da.Fill(dsProfesores, "Profesores");
+
+            pos = 0;
+            mostrarRegistro(pos);
+
+            maxRegistros = dsProfesores.Tables["Profesores"].Rows.Count;
+
+
+            con.Close();
+        }
+
+        private void BPrimero_Click(object sender, EventArgs e)
+        {
+            pos = 0;
+            mostrarRegistro(pos);
+        }
+
+        private void BAnterior_Click(object sender, EventArgs e)
+        {
+            pos--;
+            mostrarRegistro(pos);
+        }
+
+        private void BSiguiente_Click(object sender, EventArgs e)
+        {
+            pos++;
+            mostrarRegistro(pos);
+        }
+
+        private void BÚltimo_Click(object sender, EventArgs e)
+        {
+            pos = maxRegistros - 1;
+            mostrarRegistro(pos);
+        }
+    }
+}
