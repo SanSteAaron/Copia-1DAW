9d9cba86210edc1985f1cb1b2d3c688a5da4417d
SanSteAaron <SanSteAaron@gmail.com>
2022-05-17 19:50:39 +0200

    17/05

---
+++ AaronDawClase/ED/PrácticaRefactorización_AaronSanchez/Fecha/Fecha/Fecha.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fecha
+{
+    public class Fecha
+    {
+        public int day;
+        public int month;
+        public int year;
+        
+        public Fecha()
+        {
+            day = 1;
+            month = 1;
+            year = 1;
+        }
+        
+        public Fecha(int month, int year, int day)
+        {
+            if (year >= 1 && year <= 2500)
+            {
+                this.year = year;
+            }
+            else
+            {
+                this.year = 1;
+            }
+            if (month >= 1 && month <= 12)
+                this.month = month;
+            else
+                this.month = 1;
+            int diasMes = 0;
+            switch (month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    diasMes = 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    diasMes = 30;
+                    break;
+                case 2: 
+                    if (EsBisiesto() == true)
+                        diasMes = 29;
+                    else
+                        diasMes = 28;
+                    break;
+            }
+            if (day >= 1 && day <= diasMes)
+                this.day = day;
+            else
+                this.day = 1;
+        }
+        public bool EsBisiesto()
+        {
+            bool bisiesto = false;
+            if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+                bisiesto = true;
+            else bisiesto = false;
+            return bisiesto;
+        }
+
+        public override string ToString()
+        {
+            return day + "/" + month + "/" + year;
+        }
+    }
+}
---
+++ AaronDawClase/ED/PrácticaRefactorización_AaronSanchez/Fecha/Fecha/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fecha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fecha fecha1 = new Fecha(12, 2012, 4, false);
+            Console.WriteLine("Fecha 1: " + fecha1.ToString());
+            if (fecha1.EsBisiesto())
+                Console.WriteLine("El año " + fecha1.year + " es bisiesto");
+            else
+                Console.WriteLine("El año " + fecha1.year + " no es bisiesto");
+
+            Fecha fecha2 = new Fecha(10, 2013, 4, false);
+            Console.WriteLine("Fecha 2: " + fecha2.ToString());
+            if (fecha2.EsBisiesto())
+                Console.WriteLine("El año " + fecha2.year + " es bisiesto");
+            else
+                Console.WriteLine("El año " + fecha2.year + " no es bisiesto");
+
+            Fecha fecha3 = new Fecha(13, -4, 4, false);
+            Console.WriteLine("Fecha 3: " + fecha3.ToString());
+
+            Fecha fecha4 = new Fecha();
+            fecha4.day = 67;
+            fecha4.month = 80;
+            fecha4.year = 3678;
+            Console.WriteLine("Fecha 4: " + fecha4.ToString());
+        }
+    }
+}
